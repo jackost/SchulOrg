0cbce317764bed9108560f52245bdf617d6be210
Jakob <jackost93@yahoo.de>
2012-11-04 15:04:59 +0100

    .gitignore hinzugefügt

---
+++ AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SchulOrg
+//
+//  Created by Jakob on 05.10.12.
+//  Copyright (c) 2012 de.Brosu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SchulOrg
+//
+//  Created by Jakob on 05.10.12.
+//  Copyright (c) 2012 de.Brosu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
